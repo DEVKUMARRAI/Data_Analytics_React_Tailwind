1da49867db68bd42c8b9ec8b373ae387e970ae31
DEV RAI <devrai197551@gmail.com>
2024-03-18 22:48:06 +0530

    Second Command

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Navbar/>
+    <div>
+       <Navbar/>
+        <Hero/>
+    </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return ( 
+        <div className="text-white">
+            <h1>Hero</h1>
+        </div>
+     );
+}
+ 
+export default Hero;
